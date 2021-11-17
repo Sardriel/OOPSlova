e7612bf53d658620be17ba9216fccd3b0d2828b0
Sardriel <91335030+Sardriel@users.noreply.github.com>
2021-11-17 10:43:07 +0100

    Add project files.

---
+++ OOPSlova/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOPSlova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> slova = new List<string>();
+            string slovo = "";
+            
+            while (slovo !="konec")
+            {
+                Console.Write("Zadej slovo: ");
+                slovo = Console.ReadLine().ToLower();
+                if (slovo != "konec")
+                { 
+                    slova.Add(slovo);
+                }
+            }
+            Console.Write("Zadal jsi tato slova: {0}", string.Join(", ", slova.Distinct().ToArray()));
+            Console.ReadKey();
+        }
+    }
+}
